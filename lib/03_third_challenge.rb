1ffb19734c4924940a06679a7bfa7bd1d58347f1
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-25 03:52:30 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -15,9 +15,13 @@ def third_challenge
         :status => "alive"
         },
       :hero_friends => [{
-        
+        :name => "Benvolio",
+        :age => "17", 
+        :attitude => "worried"
       },{
-        
+        :name => "Mercutio",
+        :age => "18", 
+        :attitude => "hot-headed"
       }]
    }, 
    :capulet => {
@@ -35,9 +39,13 @@ def third_challenge
         :status => "alive"
         },
       :heroine_friends => [{
-        
+        :name => "Steven",
+        :age => "30", 
+        :attitude => "confused"
       },{
-        
+        :name => "Nurse",
+        :age => "44" , 
+        :attitude => "worried"
       }]
    }
   }
