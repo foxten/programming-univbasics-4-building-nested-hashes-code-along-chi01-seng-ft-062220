9e7704f73c1122e5180eb72d893e82b501586eda
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-25 03:56:42 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -15,14 +15,13 @@ def third_challenge
         :status => "alive"
         },
       :hero_friends => [{
-        name: "Benvolio",
-        age: "17", 
-        attitude: "worried"
-      },
-      {
-        name: "Mercutio",
-        age:"18", 
-        attitude: "hot-headed"
+        :name => "Benvolio",
+        :age => "17", 
+        :attitude => "worried"
+      },{
+        :name => "Mercutio",
+        :age => "18", 
+        :attitude => "hot-headed"
       }]
    }, 
    :capulet => {
@@ -40,14 +39,13 @@ def third_challenge
         :status => "alive"
         },
       :heroine_friends => [{
-        name: "Steven",
-        age: "30", 
-        attitude: "confused"
-      },
-      {
-        name: "Nurse",
-        age: "44", 
-        attitude: "worried"
+        :name => "Steven",
+        :age => "30", 
+        :attitude => "confused"
+      },{
+        :name => "Nurse",
+        :age => "44" , 
+        :attitude => "worried"
       }]
    }
   }
